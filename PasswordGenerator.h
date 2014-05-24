181c63aaf614eeb9d681a6c55fcc37b59ef04e4e
HahaTTpro <ttpro1995@yahoo.com.vn>
2014-05-24 17:00:13 +0700

    release 1.9 beta

---
+++ PasswordGenerator.h
@@ -0,0 +1,16 @@
+/*
+Password Generator
+by Haha TTpro
+*/
+class PasswordGenerator
+{
+
+public:
+
+	void generator(char *A, int n);//need fix not working
+	void Generateapassword();
+	void GenerateDigitOnly(char *Digit, int len);//on going
+	void menu();
+	void About();
+
+};
